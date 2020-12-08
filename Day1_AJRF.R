3d94111ed7903ed5dcff8d38c1e6db7477f7d1c1
Al Firrell <alastair.firrell@gmail.com>
2020-12-08 11:41:42 +0000

    Update Day1_AJRF.R
    File path update

--- Day1_AJRF.R
+++ Day1_AJRF.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 ## Part 1: 2 entries which sum to 2020
-raw_vals <- read_csv("Day01/Day1Data.csv", col_types = "i", col_names = "val")
+raw_vals <- read_csv("Day1Data.csv", col_types = "i", col_names = "val")
 raw_vals <- raw_vals$val
 
 expand_grid(val1 = raw_vals,
